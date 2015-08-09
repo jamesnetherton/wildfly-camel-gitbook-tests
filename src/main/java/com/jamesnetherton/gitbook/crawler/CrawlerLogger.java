a33b0ffb4d817cd80f6e065c24d4b9be65e72db5
James Netherton <jamesnetherton@gmail.com>
2015-08-09 17:11:18 +0100

    Use correct logging class

--- src/main/java/com/jamesnetherton/gitbook/crawler/CrawlerLogger.java
+++ src/main/java/com/jamesnetherton/gitbook/crawler/CrawlerLogger.java
@@ -4,5 +4,5 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public interface CrawlerLogger {
-    Logger log = LoggerFactory.getLogger(CrawlerRecorder.class);
+    Logger log = LoggerFactory.getLogger(CrawlerLogger.class);
 }
